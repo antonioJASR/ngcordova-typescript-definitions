eb4d7f011a49426cf8679c43e2a37b9c33d03f8e
José Antonio Sánchez Reynaga <joanzare@gmail.com>
2014-12-12 09:23:17 -0600

    add ISocialSharingService definition

---
+++ ngcordova/ngcordova.d.ts
@@ -0,0 +1,27 @@
+/// <reference path="../angularjs/angular.d.ts" />
+
+declare module ngcordova {
+    export interface ICordovaToastService {
+        show(message: string, duration: string, position: string): ng.IPromise<any>;
+        showLongBottom(message: string): ng.IPromise<any>;
+    }
+
+    export interface ISocialSharingService {
+        shareViaTwitter(message: string, image: string, link: string)
+            : ng.IPromise<any>;
+
+        shareViaWhatsApp(message: string, image: string, link: string)
+            : ng.IPromise<any>;
+            
+        shareViaFacebook(message: string, image: string, link: string)
+            : ng.IPromise<any>;
+
+        shareViaSMS(message: string, number: number): ng.IPromise<any>;
+
+        shareViaEmail(message: string, subject: string, toArr: Array<string>, 
+            bccArr: Array<string>, file: string): ng.IPromise<any>;
+
+        canShareVia(socialType: string, message: string, image: string, 
+            link: string): ng.IPromise<any>;
+    }
+}      
